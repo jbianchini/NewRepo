1801cdc65b5a113f6aec9a0e2806dd671d60f97a
Juan <jbianchini@conexia.com>
2019-12-26 16:39:05 -0300

    Adding person.java getName method and removing NewFile.xhtml

---
+++ person.java
@@ -0,0 +1,13 @@
+public class Person{
+    private String name;
+    private Integer age;
+
+    public Person(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+}
